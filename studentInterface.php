9f503b264115e5c77e5a1f2f17386f4dbc540448
staceyfras <32045253+staceyfras@users.noreply.github.com>
2018-11-25 18:13:21 -0800

    minor changes to student

--- studentInterface.php
+++ studentInterface.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Student Portal</title>
+    <title>Student Portal -- Results</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
@@ -18,8 +18,28 @@
                 INFO
             </a>
   </p>
-  <?php
 
+  <br>
+  <div class='mainform'>
+  <h3>Modify Search</h3>
+    <form action="studentInterface.php" method="post">
+                Enter CWID to get the grades of all classes taken: 
+                <input name="cwidQuery" type="text">
+                    <input type="submit" value="SUBMIT">
+                    </input>
+                </input>
+    </form>
+            <br>
+      <form action="studentInterface.php" method="post">
+          Enter course number to get all sections of that course: 
+          <input name="courseQuery" type="text">
+              <input type="submit" value="SUBMIT">
+              </input>
+          </input>
+      </form>
+    </div>
+
+  <?php
     // Create connection for csuf server
     //conn = new mysqli("ecsmysql", "cs332a20", "hievoosi", "database_name")
 
@@ -32,15 +52,15 @@
 
     function get_query($conn)
     {
+        //Query: Display Grades + Classes of given CWID
         if (isset($_POST['cwidQuery'])) {
-            $stringOfQuery = "SELECT * FROM STUDENTS WHERE sCWID = '" . $_POST['cwidQuery'] . "'";
+            $stringOfQuery = "SELECT cTitle, rSecNum, rGrade from RECORDS INNER JOIN SECTIONS on RECORDS.rSecNum = SECTIONS.Snum INNER JOIN COURSES on SECTIONS.sCourseNum = COURSES.cNum WHERE RECORDS.rCWID = '" . $_POST['cwidQuery'] . "'";
             $sqlresult = $conn->query($stringOfQuery);
         }
         //These if statements are needed to determine which form was filled
-        if (isset($_POST['classQuery'])) {
+        if (isset($_POST['courseQuery'])) {
             //placeholder function
-            $stringOfQuery = "SELECT * FROM STUDENTS ";
-            $stringOfQuery .= "where sFName = '" . $_POST['nameQuery'] . "'";
+            $stringOfQuery = "SELECT * from SECTIONS INNER JOIN COURSES WHERE cNum = '" . $_POST['courseQuery'] . "'";
             $sqlresult = $conn->query($stringOfQuery);
         }
         return $sqlresult;
@@ -77,20 +97,19 @@
     } //END FUNCTION
 
   ?>
-
-        <div style="text-align:left" class='table'>
-              <?php echo "YOUR CWID:"; ?>
-              <?php echo $_POST["cwidQuery"]; ?>
-          <div style='width: 1000px' class='cell mainform'>
-
+  <h3>Results</h3>
+  <div style="text-align:left" class='table'>   
   <?php
     $sqlresult = get_query($conn);
+    
     if ($sqlresult != null) { echo sql_to_html_table($sqlresult, $delim = "\n"); } 
     else { echo "Failed to print" . "\n"; }
+    
     mysqli_close($conn);
   ?>
-          </div>
-        </div>
+
+  </div>
+        
 </body>
 </html>
 
